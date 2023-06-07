208c8c5063d19bd78d0248bac0ae352c5161dc17
Ricardo Rosendo <124933384+rickicr-collab@users.noreply.github.com>
2023-06-07 18:10:19 -0300

    Add files via upload

--- desafio25.py
+++ desafio25.py
@@ -1,3 +1,3 @@
 #crie um programa que leia o nome de uma pessoa e diga se ela tem o nome 'SILVA' no nome. 
-nome = str(input('Digite seu nome:')).strip() # criado variavel nome com classe primitiva string / aplicando função strip para eliminar espacos esquerdos e direitos desnecessarios
-print('Seu nome tem silva:{}'.format('silva' in nome.capitalize())) # imprimindo informaçoes da variavel nome com função In nome.capitalize() para realizar a formatação no nome ao ser inserido com função capitalize 
+nome = str(input('Digite seu nome:')).strip().capitalize() # criado variavel nome com classe primitiva string / aplicando função strip para eliminar espacos esquerdos e direitos desnecessarios
+print('Seu nome tem silva:{}'.format('silva' in nome)) # imprimindo informaçoes da variavel nome com função In nome.capitalize() para realizar a formatação no nome ao ser inserido com função capitalize 
