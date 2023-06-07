aa20691fa92abd99d8adf78e6af98a1af5e39e3e
Ricardo Rosendo <124933384+rickicr-collab@users.noreply.github.com>
2023-06-07 18:33:34 -0300

    Add files via upload

--- desafio36.py
+++ desafio36.py
@@ -9,19 +9,19 @@ periodo = tempo * 12
 plano = valor/periodo
 
 if plano > salario:
-  print(f"""Infelizmente, o seu salário é \033[1;31minsuficiente\033[m para pagar a casa no tempo desejado
+    print(f"""Infelizmente, o seu salário é \033[1;31minsuficiente\033[m para pagar a casa no tempo desejado
 Não podemos te oferecer esse empréstimo
 \033[7;31mValor da prestação: {plano:.2f}R$\033[m
 \033[7;31mQuantia faltando p/ prestação: {periodo-salario:.2f}R$\033[m""")
   
 elif plano > 0.3 * salario:
-  print(f"""Infelizmente, o valor da prestação equivale a \033[1;31mmais de 30% do salário\033[m
+    print(f"""Infelizmente, o valor da prestação equivale a \033[1;31mmais de 30% do salário\033[m
 Portanto, não podemos te oferecer esse empréstimo
 \033[7;31mValor da prestação: {plano:.2f}R$\033[m
 \033[7;31m% do salário que a prestação representa: {plano / salario *100:.2f}%\033[m""")
   
 else:
-   print(f"""\033[1;32mNão há objeções contra o seu empréstimo!\033[m
+    print(f"""\033[1;32mNão há objeções contra o seu empréstimo!\033[m
 \033[42mValor da prestação: {plano:.2f}R$\033[m""")
   
 
