5a000755ba2816c945204852daa22761e1e98d9f
Ricardo Rosendo <124933384+rickicr-collab@users.noreply.github.com>
2023-06-07 18:15:45 -0300

    Add files via upload

--- desafio26.py
+++ desafio26.py
@@ -3,6 +3,6 @@
 #em que posição ela apareçe na primeira vez
 #em que posição ela aparece na ultima vez 
 frase = str(input('Digite uma frase:')).upper().strip() # criado variavel comm string e aplicado tambem a função .upper() e .strip()
-print(' A letra {} apareçe na frase : {} vezes'.format(frase, frase.count('A'))) # imprimir o a frase utilizando a função .count('A)
+print(' Na frase {} a letra A apareçe : {} vezes'.format(frase, frase.count('A'))) # imprimir o a frase utilizando a função .count('A)
 print(' A primeira letra A aparece na posição : {}'.format(frase.find('A')+1)) #imrpimir a posição da primeira A utilizando .find('A') +1 para considerar o valor da posição como acrescentar mais 1
 print(' A ultima letra A apareçe na posição : {}'.format(frase.rfind('A')+1)) #imprimir a posição da ultima letra A utilizando .rfind('A") +1 para considerar o valor da posição como acrescentar mais 1
