ff7659bcff55968b08973e18e59379e8c7895903
Ricardo Rosendo <124933384+rickicr-collab@users.noreply.github.com>
2023-06-07 18:33:06 -0300

    Add files via upload

--- desafio35.py
+++ desafio35.py
@@ -6,9 +6,9 @@ r1 = float(input('Primeiro segmento:'))
 r2 = float(input('Segundo segmento:'))
 r3 = float(input('Terceiro segmento:'))
 if r1 < r2 + r3 and r2 < r1 + r3 and r3< r1 + r2:
-  print('Os segmentos acima PODEM FORMAR TRIÂNGULO!')
+    print('Os segmentos acima PODEM FORMAR TRIÂNGULO!')
 else:
-  print('OS segmentos acima NÂO PODEM FORMAR TRIÂNGULO!')
+    print('OS segmentos acima NÂO PODEM FORMAR TRIÂNGULO!')
 
 
 
