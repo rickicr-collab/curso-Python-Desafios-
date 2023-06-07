623f184789b8744754dd89c45cb10973c0fec1fd
Ricardo Rosendo <124933384+rickicr-collab@users.noreply.github.com>
2023-06-07 18:26:02 -0300

    Add files via upload

--- desafio31.py
+++ desafio31.py
@@ -4,10 +4,10 @@
 distancia = float(input('Qual é a distancia da sua viagem?'))
 print('você terá uma viagem de {:.2f}km !'.format(distancia))
 if distancia <= 200:
-  preço1 = distancia * 0.50
-  print('sua viagem de {} km  terá valor a pagar de {:.2f}'.format(distancia, preço1))
+    preço1 = distancia * 0.50
+    print('sua viagem de {} km  terá valor a pagar de {:.2f}'.format(distancia, preço1))
 else:
-  preço2 = distancia * 0.45
-  print('sua viagem de {} km terá valor a pagar de {:.2f}'.format(distancia, preço2))
-print('Tenha uma boa viagem! é obrigado por escolher nossa empresa')
-  
+    preço2 = distancia * 0.45
+    print('sua viagem de {} km terá valor a pagar de {:.2f}'.format(distancia, preço2))
+    print('Tenha uma boa viagem! é obrigado por escolher nossa empresa')
+    
