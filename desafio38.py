65a0d4004f772e40e382219f4ac6c72b3fcfb92f
Ricardo Rosendo <124933384+rickicr-collab@users.noreply.github.com>
2023-06-08 09:37:34 -0300

    Add files via upload

--- desafio38.py
+++ desafio38.py
@@ -6,9 +6,8 @@ numero1 = int(input('Digite o primeiro numero:')) # variavel numero1 criada com 
 numero2 = int(input('Digite o segundo numero:')) # variavel numero2 criada com classe primitiva inteiro por ser numero
 
 if numero1 > numero2: # condição if informando que variavel numero1 é maior que variavel numero2
-  print('O primeiro numero é maior')
+    print('O primeiro numero é maior')
 elif numero1 < numero2: # condição elif informando que variavel numero1 é menor que variavel numero2
-  print('O segundo numero é maior')
+    print('O segundo numero é maior')
 elif numero1 == numero2: # condição elif informando que as variaveis possuem valores iguais 
-  print('Não existe valor maior os dois são iguais')
-  
+    print('Não existe valor maior os dois são iguais')
--- desafio39.py
+++ desafio39.py
@@ -9,17 +9,17 @@ nascimento = int(input('Qual ano de nascimento?'))
 idade = atual - nascimento
 print('Quem nasceu {} tem {} anos em {}'.format(nascimento,idade, atual))
 if idade == 18:
-  print('\033[1;33mVocê esta com {} anos se aliste Imediatamente!\033[m '.format(idade))
+    print('\033[1;33mVocê esta com {} anos se aliste Imediatamente!\033[m '.format(idade))
 elif idade < 18:
-  saldo = 18 - idade
-  print('\033[1;32mAinda falta {} anos para o alistmento !\033[m'.format(saldo))
-  ano = atual + saldo
-  print('\033[1;32mSeu alistamento será {}\033[m'.format(ano))
+    saldo = 18 - idade
+    print('\033[1;32mAinda falta {} anos para o alistmento !\033[m'.format(saldo))
+    ano = atual + saldo
+    print('\033[1;32mSeu alistamento será {}\033[m'.format(ano))
 elif idade > 18:
-  saldo = idade - 18
-  print('\033[1;31mVocê ja deveria ter se alistado a {} anos !!!\033[m '.format(saldo))
-  alistamento = atual - 18
-  print('\033[1;31mseu alistamento foi em {}\033[m'.format(alistamento))
+    saldo = idade - 18
+    print('\033[1;31mVocê ja deveria ter se alistado a {} anos !!!\033[m '.format(saldo))
+    alistamento = atual - 18
+    print('\033[1;31mseu alistamento foi em {}\033[m'.format(alistamento))
   
 
 
